ff0a639347bc88532d0c6871b16ed31b9b91d313
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-06-20 16:47:02 +0100

    Header and navigation bar complete.

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,16 @@
-
+$(document).ready(function() {
+  $('#about_ea').hover(
+    function () {
+      $('ul', this).slideDown("fast");
+    }, function () {
+      $('ul', this).slideUp("fast");
+    }
+  );
+  $('#our_programmes').hover(
+    function () {
+      $('ul', this).slideDown("fast");
+    }, function () {
+      $('ul', this).slideUp("fast");
+    }
+  );
+});
