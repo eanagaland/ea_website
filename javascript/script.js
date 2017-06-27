d138555e2db6cc14646ca5c3f3b70b0ee98199d3
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-06-27 14:21:23 +0100

    Styled and tabulated team page. Offline so cant test jquery at the moment. Probably need to add a footer at some point.

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
   $('#about_ea').hover(
     function () {
-      $('ul', this).slideDown("fast");
+      $('ul', this).slideDown('fast');
     }, function () {
-      $('ul', this).slideUp("fast");
+      $('ul', this).slideUp('fast');
     }
   );
   $('#our_programmes').hover(
     function () {
-      $('ul', this).slideDown("fast");
+      $('ul', this).slideDown('fast');
     }, function () {
-      $('ul', this).slideUp("fast");
+      $('ul', this).slideUp('fast');
     }
   );
+
+  $('#team_table th').on('click', function() {
+    teams = {'mn': 'management_team',
+             'kh': 'kohima_ho',
+             'ft': 'finance_team',
+             'dt': 'dimapur_team',
+             'mt': 'mok_team',
+             'pt': 'pfo_team'};
+    active_team = $('.active_team');
+    /* Hide deselected team div, and set to inactive class */
+    $('#' + teams[active_team.id]).css('display', 'none');
+    active_team.removeClass('active_team');
+    active_team.addClass('inactive_team');
+    /* Show selected team div, and set to active class */
+    $('#' + teams[$(this).id]).css('display', 'block');
+    $(this).removeClass('inactive_team');
+    $(this).addClass('active_team');
+  });
 });
